6f451bc8145a36dbf2597c7bcb446214478d9ee8
Sheuh Xiong <sheuhxiong@gmail.com>
2021-01-17 13:33:02 -0500

    Image author link redirects to image source

--- src/components/GameStart/GameStart.jsx
+++ src/components/GameStart/GameStart.jsx
@@ -15,11 +15,17 @@ function GameStart({ setGameStarted, setTimerOn }) {
         <div className="left-side-modal">
           <h1>Find <span className="dio-span">DIO</span></h1>
           <img className="jojo-img" src={jojoImg} alt=""/>
-          <p>Help the Joestars find their enemy <span className="dio-span">DIO</span> and his two minions, <span className="ed-span">Edward Elric</span> and <span className="spike-span">Spike Spiegel</span></p>
+          <p>
+            Help the Joestars find their enemy <span className="dio-span">DIO</span> and his two minions, <span className="ed-span">Edward Elric</span> and <span className="spike-span">Spike Spiegel</span>
+          </p>
         </div>
         <div className="right-side-modal">
           <button onClick={startGame}>Start</button>
-          <p className="artist"><a href="https://www.artstation.com/chekavo">Image by Egor Klyuchnyk</a></p>
+          <p className="artist">
+            <a href="https://www.artstation.com/artwork/nQgAEK" target="blank">
+              Image by Egor Klyuchnyk
+            </a>
+          </p>
         </div>
       </div>
     </div>
